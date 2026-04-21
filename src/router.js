9489411b164a4e33297571208f91fdd1b03bedf9
Serap Aydin <128484094+seay28@users.noreply.github.com>
2026-04-21 21:13:51 +0200

    fix last one

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory  } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import Homepage from "@/Layout/Homepage.vue"
 import AboutMe from "@/Layout/AboutMe.vue"
@@ -9,7 +9,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHashHistory('/portfolio/'),
+    history: createWebHashHistory(),
     routes,
 })
 
